e41e377cf4df1f2373494ded915d629a7352bc63
Joshua Shepard <joshua_shepard@yahoo.com>
2025-05-16 19:53:44 -0400

    Event listener and logging user input to console. Add Comments

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const userForm = document.getElementById('city-form');
 
 /*
     Event listener for form submission
-    Call preventDefault() on submission to stop refresh on submission.
+    Call preventDefault() on event to stop refresh on submission.
 */ 
 userForm.addEventListener('submit', e => {
+    // Stops page from refreshing
     e.preventDefault();
 
     // Input Element - used to grab user input
     const userInputElement = document.getElementById('city-search');
     // Save user input 
     const userInput = userInputElement.value;
-    
+
     console.log(userInput);
 });
